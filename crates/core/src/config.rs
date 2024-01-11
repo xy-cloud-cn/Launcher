a06e2c2e8b605d778fa0acc8cf7c0671feb07656
xy_cloud <85053859+xy-cloud-cn@users.noreply.github.com>
2024-01-11 19:14:23 +0800

    Adapted LiteLoader

--- crates/core/src/config.rs
+++ crates/core/src/config.rs
@@ -10,16 +10,15 @@ use crate::matcher::SourceMatcherEnum;
 use crate::processor::SourceProcessor;
 
 #[derive(Deserialize, Debug)]
-pub(crate) struct ConfigRule {
-    pub(crate) matcher: Box<SourceMatcherEnum>,
-    pub(crate) processors: Vec<SourceProcessor>,
+pub(crate) struct Config {
+    pub(crate) identifiers: Identifiers,
+    pub(crate) rules: HashMap<String, ConfigRule>,
 }
 
 #[derive(Deserialize, Debug)]
-pub(crate) struct Config {
-    #[serde(default)]
-    pub(crate) identifiers: Identifiers,
-    pub(crate) rules: HashMap<String, ConfigRule>,
+pub(crate) struct ConfigRule {
+    pub(crate) matcher: Box<SourceMatcherEnum>,
+    pub(crate) processors: Vec<SourceProcessor>,
 }
 
 pub(crate) trait ReadFromFile {
@@ -33,11 +32,13 @@ impl ReadFromFile for Config {
         file.read_to_string(&mut file_content)
             .expect("read config file failed");
         let data: Value = serde_json::from_str(file_content.as_str()).expect("parse config file failed");
+        println!("{:?}", data);
         // 你可以使用下标操作符来访问json对象的Launcher字段
         // 你可以使用clone方法来复制一个值类型，或者使用take方法来转移所有权
         let launcher = data["Launcher"].clone();
         // 你可以使用serde_json::from_value函数来把它转换成Config类型
         let config: Config = serde_json::from_value(launcher).expect("convert to Config failed");
+
         config
 
 
